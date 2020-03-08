ab686ff3bf8fdb928d4e2be8058a1c614a58cf7b
boopper <samantha.sjacobs@gmail.com>
2020-03-08 12:55:48 -0700

    initial checkin

--- donor_runner.py
+++ donor_runner.py
@@ -10,9 +10,46 @@ def parse_line(line:str) -> Tuple[str]:
 
 
 if __name__ == '__main__':
+
+    #print (len(sys.argv), sys.argv,sys.argv[1])
+    cmd = ""
+    amount = 0
+
+    if len(sys.argv)  < 3:
+        print ("Not enough command line arugments:",sys.argv)
+        exit()
+    else:
+        file_name = sys.argv[1]   
+        if sys.argv[2] == 'all':
+            cmd = 'all'
+        elif sys.argv[2] == 'cheap':
+            cmd = 'cheap'
+        elif sys.argv[2] == 'rich':
+            cmd = 'rich'
+        elif sys.argv[2] == 'who':
+            if len(sys.argv) == 4:     # extra who arg
+                cmdstring = sys.argv[3]
+                if cmdstring[0] == '+':
+                    cmd = 'list_above'
+                    amount = int(cmdstring)
+                elif cmdstring[0] == '-':
+                    cmd = 'list_below'
+                    amount = int(cmdstring)*-1
+                else:
+                    cmd = 'who'
+                    amount = int(cmdstring)
+            else:
+                print ("Wrong argument given for who cmmand")
+                exit()
+
+        else:
+            print ("Wrong aurgments given:", sys.argv)
+            exit()
+
+    #print("CMD and amount",cmd,amount) 
     donors = []
 
-    file_name = "Trees/src/donor_prog/donor_files/donor10.txt"
+    #file_name = "Trees/src/donor_prog/donor_files/donor10.txt"
     with open ( file_name ) as file:
 
         for line in file:
@@ -20,22 +57,27 @@ if __name__ == '__main__':
             donors.append(donor)
 
 
-    for donor in donors:
-        print (donor)
+    #for donor in donors:
+    #    print (donor)
 
     tree = BST()
 
     for donor in donors:
         tree.add_value (donor)
 
+    if cmd == 'rich':
+        print (tree.get_max_node().value)
+    elif  cmd == 'cheap':
+        print (tree.get_min_node().value)
+    elif  cmd == 'all':
+        for x in tree:
+            print (x)
+        
     print ("find len",len(tree))
     print ("find height",tree.height())
     print ("find 19",tree.get_node(19).value)
+    print ("cheap",tree.get_min_node().value)
+    print ("rich",tree.get_max_node().value)
     print ("find 1742",tree.get_node(1742))
 
-
-    print (len(sys.argv), sys.argv,sys.argv[1])
-
-
-#    if len(sys.argv) == 2:
 
